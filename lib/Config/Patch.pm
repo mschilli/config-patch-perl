b92561803ef1f750f6919f37186206247ce0721d
mschilli <github@perlmeister.com>
2005-07-16 21:22:31 +0000

    ready for cpan

--- lib/Config/Patch.pm
+++ lib/Config/Patch.pm
@@ -10,7 +10,7 @@ use strict;
 use warnings;
 use MIME::Base64;
 
-our $VERSION     = "0.01";
+our $VERSION     = "0.02";
 our $PATCH_REGEX = qr{^#\(Config::Patch-(.*?)-(.*?)\)}m;
 
 ###########################################
